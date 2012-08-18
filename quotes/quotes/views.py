d7a0192da6d2efd80a00af3d8f4e5e237b18c850
Patrick Arminio <patrick.arminio@gmail.com>
2012-08-18 17:43:53 +0200

    removed hit counter for the moment

--- quotes/quotes/views.py
+++ quotes/quotes/views.py
@@ -6,16 +6,9 @@ from braces.views import LoginRequiredMixin
 from .models import Quote
 from .forms import QuoteForm
 
-from mongo_hit_counter.utils import insert_hit
-
 
 class QuoteDetail(DetailView):
     model = Quote
-
-    def get(self, request, *args, **kwargs):
-        insert_hit('Quote', str(self.model.pk), request.META['REMOTE_ADDR'])
-
-        return super(QuoteDetail, self).get(request, *args, **kwargs)
 
 
 class QuoteCreate(LoginRequiredMixin, CreateView):
