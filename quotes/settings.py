ffbaab7800234f59d039a4954d1f3b5177d68a25
Patrick Arminio <patrick.arminio@gmail.com>
2012-08-18 03:48:45 +0200

    Added django storages.

--- quotes/settings.py
+++ quotes/settings.py
@@ -1,3 +1,4 @@
+import os
 from os.path import dirname, join, realpath
 
 import django
@@ -10,7 +11,12 @@ SITE_ROOT = dirname(dirname(realpath(__file__)))
 
 path = lambda *args: join(SITE_ROOT, *args)
 
-DEBUG = True
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'citation_needed')
+AWS_QUERYSTRING_AUTH = False
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -47,21 +53,10 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = path('media')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = path('static')
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = STATIC_URL + 'media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -120,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
 
     'south',
+    'storages',
 )
 
 # A sample logging configuration. The only tangible logging
