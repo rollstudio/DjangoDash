6ba4128763fc3c168b07dd8984aec2c98771f4d6
Patrick Arminio <patrick.arminio@gmail.com>
2012-08-19 00:42:22 +0200

    No more hard coded form

--- quotes/quotes/forms.py
+++ quotes/quotes/forms.py
@@ -9,3 +9,9 @@ class QuoteForm(forms.ModelForm):
     class Meta:
         model = Quote
         exclude = ('user', 'metadata', 'author')
+
+    def __init__(self, *args, **kwargs):
+        super(QuoteForm, self).__init__(*args, **kwargs)
+        for label, field in self.fields.items():
+            field.widget.attrs['placeholder'] = field.help_text
+            field.help_text = None
