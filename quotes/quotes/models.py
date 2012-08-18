9fe06c33059493b57ed1c4f5939b2a04e4cf3809
Germano Gabbianelli <tyrion.mx@gmail.com>
2012-08-18 04:31:52 +0200

    Added basic models.

--- quotes/quotes/models.py
+++ quotes/quotes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Quote(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    body = models.TextField()
+
+    user = models.ForeignKey(User, related_name='quotes', blank=True, null=True)
+    author = models.ForeignKey(Author, related_name='quotes')
+
+    language = models.CharField(max_length=2, choices=settings.LANGUAGES)
