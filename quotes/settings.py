54227e55580fc7f70d39c07603dab5bab2364923
Patrick Arminio <patrick.arminio@gmail.com>
2012-08-18 04:18:16 +0200

    Forgot to set settings for storages.

--- quotes/settings.py
+++ quotes/settings.py
@@ -147,6 +147,9 @@ LOGGING = {
     }
 }
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
 try:
     from local_settings import *
 except ImportError:
