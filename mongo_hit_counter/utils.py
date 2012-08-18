2495798a5b89ea3238b7f42e0a05b10bb0c202a4
Patrick Arminio <patrick.arminio@gmail.com>
2012-08-18 17:22:00 +0200

    Added simple hit counter

--- mongo_hit_counter/utils.py
+++ mongo_hit_counter/utils.py
@@ -1,1 +1,33 @@
-# Create your views here.
+from pymongo import Connection
+
+connection = Connection()  # TODO: Get string from settings
+
+db = connection.hit_counter
+
+hits = db.hits
+single_hits = db.single_hits
+
+
+def insert_hit(content_type, id, ip):
+    h = hits.find({
+        'contentType': content_type,
+        'objId': id,
+        'ip': ip
+    })
+
+    print h.count()
+    print
+
+    if h.count() == 0:
+        single_hits.update({
+            'contentType': content_type,
+            'objId': id,
+        }, {
+            '$inc': {'hits': 1}
+        }, True)
+
+    hits.insert({
+        'contentType': content_type,
+        'objId': id,
+        'ip': ip
+    })
