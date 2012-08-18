5c1ab6baab57424a442337204034a0a3f04d15a5
Germano Gabbianelli <tyrion.mx@gmail.com>
2012-08-18 06:23:04 +0200

    Started to add some views.

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, RedirectView
 
+from quotes.models import Quote
+
 admin.autodiscover()
 
 
@@ -16,6 +18,11 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^coming$', ComingSoonView.as_view()),
     url(r'^$', HomeView.as_view()),
+    url(r'^add$', 'django.views.generic.create_update.create_object',
+        {'model': Quote}),
+    #url(r'^show/(?P<object_id>\d+)$',
+    #    'django.views.generic.list_detail.object_detail',
+    #    {'queryset': Quote.objects.all()}),
     # url(r'^quotes/', include('quotes.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
